2a02289e0da660bf0b832c9a746f73a66340ae79
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-05-05 19:28:30 -0400

    Add spinner when loading categories

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,6 +1,5 @@
 import "./category-preview.styles.scss";
 import ProductCard from "../product-card/product-card.component";
-
 import { Link } from "react-router-dom";
 
 const CategoryPreview = ({ title, products }) => {
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,13 +1,21 @@
-import "./category.styles.scss";
-import React, { Fragment, useEffect, useState } from "react";
-import { useParams } from "react-router";
-import ProductCard from "../../components/product-card/product-card.component";
+import { useState, useEffect, Fragment } from "react";
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import { useParams } from "react-router-dom";
+
+import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
+
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from "../../store/categories/category.selector";
+
+import { CategoryContainer, Title } from "./category.styles";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -16,13 +24,17 @@ const Category = () => {
 
   return (
     <Fragment>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
+      <Title>{category.toUpperCase()}</Title>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
   );
 };
