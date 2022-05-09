cffeb98113163f08a2413ced43d5441c4d994ae0
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-05-09 18:37:06 -0400

    Downgrade React in order to implement Stripe API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
@@ -8,15 +8,16 @@ import App from "./App";
 import "./index.scss";
 import { store, persistor } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const entryPoint = document.getElementById("root");
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  entryPoint
 );
